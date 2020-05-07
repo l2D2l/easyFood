177cf73e13e5c311e2692384103b88f36ca99f9f
Jeancarlos <jeancarlos.rafael@sentz.io>
2020-05-07 18:04:10 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  latitude: any = 0; //latitude
+  longitude: any = 0; //longitude
 
-  constructor() {}
+  constructor(
+    private geolocation: Geolocation
+  ) {}
 
+  options = {
+    timeout: 10000, 
+    enableHighAccuracy: true, 
+    maximumAge: 3600
+  };
+
+  // use geolocation to get user's device coordinates
+  getCurrentCoordinates() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
 }
